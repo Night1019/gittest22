829bb2d3530879ed7cca688983d806aa9986fd65
ZHAO911 <903612473@qq.com>
2024-06-11 09:06:47 +0800

    222222222222222222

--- src/t1.java
+++ src/t1.java
@@ -1,14 +1,6 @@
 public class t1 {
 
     public static void main(String[] args) {
-        System.out.println("ceshicccc!");
-        System.out.println("t1111111");
-
         System.out.println("测试测试s");
-
-        System.out.println("1222222");
-        System.out.println("4444444444");
-        System.out.println("555");
-
     }
 }
